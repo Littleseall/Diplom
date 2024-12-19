733162c4682874413f3d01ba77a72ad059a4bdd2
Olga Lukunina <oshr8p@yandex.ru>
2024-12-19 20:43:22 +0300

    commit

---
+++ spd-diplom-main/social_network/posts/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Post, Comment
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['author', 'text', 'created_at']
+
+class PostSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True, read_only=True)
+    likes_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Post
+        fields = ['id', 'text', 'image', 'created_at', 'comments', 'likes_count']
+
+
+    def get_likes_count(self, obj):
+        return obj.likes.count()
